f85486a05938ab829701ce4477257f4e46ce39d9
Manav Kaushal <manavkaushal756@gmail.com>
2022-01-18 12:09:53 +0530

    chart complete

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -15,7 +15,11 @@ import {
   ThunderboltOutlined,
 } from "@ant-design/icons";
 
-import { useGetCryptoDetailsQuery } from "../services/cryptoApi";
+import {
+  useGetCryptoDetailsQuery,
+  useGetCryptoHistoryQuery,
+} from "../services/cryptoApi";
+import LineChart from "./LineChart";
 
 const { Title, Text } = Typography;
 const { Option } = Select;
@@ -24,9 +28,16 @@ const CryptoDetails = () => {
   const { coinId } = useParams();
   const [timePeriod, setTimePeriod] = useState("7d");
   const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
+  const { data: coinHistory } = useGetCryptoHistoryQuery({
+    coinId,
+    timePeriod,
+  });
   const cryptoDetails = data?.data?.coin;
 
-  console.log(cryptoDetails);
+  console.log("timePeriod: ", timePeriod);
+  console.log("coinHistory: ", coinHistory);
+
+  if (isFetching) return "Loading...";
 
   const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"];
 
@@ -120,6 +131,11 @@ const CryptoDetails = () => {
         </Select>
 
         {/* Line Chart */}
+        <LineChart
+          coinHistory={coinHistory}
+          currentPrice={millify(cryptoDetails?.price)}
+          coinName={cryptoDetails?.name}
+        />
 
         <Col className="stats-container">
           <Col className="coin-value-statistics">
